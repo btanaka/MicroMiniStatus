c9e14e0d5ff439df7917980ff3a9fd308a80aa56
Brian Tanaka <brian.tanaka@gmail.com>
2010-06-06 12:53:05 -0700

    first go

--- microministatus.sh
+++ microministatus.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-# mi
+# microministatus - personal status in the shell.
+#   useful for stuff like "staff meeting's coming... what
+#   did i do this week? oh yeah, i can just
+#   skim my statuses."
+#   this script just writes. to read, just use your $PAGER or text editor.
+
+#
+# configuration
+#
+MMSLOG="$HOME/Desktop/mmslog.txt"
+
+#
+# do it
+#
+NOW=`date`
+separator="-------------------- $NOW"
+echo "What are you doing? Just did? About to do? Etc."
+read ANSWER
+echo $separator >> $MMSLOG
+echo $ANSWER >> $MMSLOG
+tail $MMSLOG
